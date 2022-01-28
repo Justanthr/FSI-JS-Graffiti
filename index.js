d282e58be28e948bcb2670f71f6c650f7263d6fa
Jennika Wilson <j.r.a.112100@gmail.com>
2022-01-27 19:14:36 -0500

    finished project

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Select some elements...
+let pageHeader = document.querySelector('#page-header')
+pageHeader.style.backgroundColor = 'black'
+let dogNames = document.querySelectorAll('.dog-name');
+for (let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.color = 'purple'
+}
+let dogImages = document.querySelectorAll('.dog-image')
+for (let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '15px'
+}
